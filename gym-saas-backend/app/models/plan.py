40ec9e48c70c33cd7ba18cae898f81bda1573bfa
ziadabdullah007 <zyadabdallah11@gmail.com>
2026-06-12 18:23:14 +0300

    Refactor Plan model and add new plan routes, repository, and service files

--- gym-saas-backend/app/models/plan.py
+++ gym-saas-backend/app/models/plan.py
@@ -1,26 +1,27 @@
 import uuid
 
-from sqlalchemy import String, Text, Numeric
+from sqlalchemy import ForeignKey, String, Text, Numeric
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import relationship
 from app.db.base import Base
-
+from sqlalchemy import ForeignKey
 
 class Plan(Base):
     __tablename__ = "plans"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True
+    UUID(as_uuid=True),
+    primary_key=True,
+    default=uuid.uuid4
     )
     subscriptions = relationship(
         "Subscription",
         back_populates="plan"
 )
     gym_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True)
-    )
+    ForeignKey("gyms.id")
+)
 
     name: Mapped[str] = mapped_column(
         String(255)
